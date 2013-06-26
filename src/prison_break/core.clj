93900315946088b6bb64f121f43b84fca37b3008
Schalk <schalk.work@gmail.com>
2013-06-26 12:18:19 +0000

    add funcs file

--- src/prison_break/core.clj
+++ src/prison_break/core.clj
@@ -1,9 +1,12 @@
 (ns prison-break.core
   (:gen-class))
 
+(load "funcs")
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (println "Prison Break!")
+  (println action))
---
+++ src/prison_break/funcs.clj
@@ -0,0 +1,10 @@
+(ns prison-break.core)
+
+(def action [:cooperate :defect :random])
+
+(def history-action [:cooperate :defect :none :ignore])
+
+
+
+
+
